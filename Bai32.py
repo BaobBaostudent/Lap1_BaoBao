3c993cbecc5ae293fc0451d093a9633440c8fdf2
BaobBaostudent <giab7954@gmail.com>
2024-09-22 16:01:11 +0700

    Update Bai32.py

--- Bai32.py
+++ Bai32.py
@@ -4,16 +4,15 @@ Created on Sun Sep 22 14:29:31 2024
 
 @author: ADMIN
 """
-
 a = float(input("Nhập vào số Km: "))
 if a <= 1:
     st = 15  
     print("Số tiền phải trả là: ", st);
-elif 2 <= a <= 5:
+elif 1 < a <= 5:
     st= 1*15 + (a-1)*13.5 
     print("Số tiền phải trả là: ", st);
-elif a > 6:
-    st=1*15 + (a-1)*13.5 + (a-6)*11 
+elif a >= 6:
+    st=1*15 + 4*13.5 + (a-5)*11 
     print("Số tiền phải trả là: ", st);
 if a > 120:
     stc = st*0.9 
